97940c479997b4ea685fc10221dcaa5e836c93f2
ChiragBhutaniOS <chirag.bhutani@nagarro.com>
2020-05-08 23:32:01 +0530

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Chirag Bhutani on 08/05/20.
+//  Copyright © 2020 Chirag Bhutani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
